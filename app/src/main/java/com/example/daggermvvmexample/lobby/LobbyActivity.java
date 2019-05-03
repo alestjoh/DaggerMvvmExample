ee50b1c170f1164660679f44c54a3b96e3258488
Lyndis Mushin <alestjoh@gmail.com>
2019-05-03 00:05:57 -0400

    Implemented LobbyActivity

--- app/src/main/java/com/example/daggermvvmexample/lobby/LobbyActivity.java
+++ app/src/main/java/com/example/daggermvvmexample/lobby/LobbyActivity.java
@@ -1,15 +1,90 @@
 package com.example.daggermvvmexample.lobby;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.daggermvvmexample.R;
+import com.example.daggermvvmexample.common.viewmodel.Response;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import dagger.android.AndroidInjection;
+import timber.log.Timber;
 
 public class LobbyActivity extends AppCompatActivity {
 
+    @Inject
+    LobbyViewModelFactory viewModelFactory;
+
+    @BindView(R.id.greeting_textview)
+    TextView greetingTextView;
+
+    @BindView(R.id.loading_indicator)
+    ProgressBar loadingIndicator;
+
+    private LobbyViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(LobbyViewModel.class);
+        viewModel.response().observe(this, response -> {});
+    }
+
+    @OnClick(R.id.common_greeting_button)
+    void onCommonGreetingButtonClicked() {
+        viewModel.loadCommonGreeting();
+    }
+
+    @OnClick(R.id.lobby_greeting_button)
+    void onLobbyGreetingButtonClicked() {
+        viewModel.loadLobbyGreeting();
+    }
+
+    private void processResponse(Response response) {
+        switch (response.status) {
+            case LOADING:
+                renderLoadingState();
+                break;
+
+            case SUCCESS:
+                renderDataState(response.data);
+                break;
+
+            case ERROR:
+                renderErrorState(response.error);
+                break;
+        }
+    }
+
+    private void renderLoadingState() {
+        greetingTextView.setVisibility(View.GONE);
+        loadingIndicator.setVisibility(View.VISIBLE);
+    }
+
+    private void renderDataState(String greeting) {
+        loadingIndicator.setVisibility(View.GONE);
+        greetingTextView.setVisibility(View.VISIBLE);
+        greetingTextView.setText(greeting);
+    }
+
+    private void renderErrorState(Throwable throwable) {
+        Timber.e(throwable);
+        loadingIndicator.setVisibility(View.GONE);
+        greetingTextView.setVisibility(View.GONE);
+        Toast.makeText(this, R.string.greeting_error, Toast.LENGTH_SHORT).show();
     }
 }
